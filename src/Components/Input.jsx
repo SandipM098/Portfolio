54e55cc0fd644484277dc05fc48911ce9aac165a
Sandip Magar <reone054@gmail.com>
2025-09-02 17:38:43 +0545

    contact page

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
-
-const Input = () => {
+import { useTheme } from '../context/ThemeContext';
+const Input = ({type, placeholder}) => {
+    const { theme } = useTheme();
   return (
-    <div>Input</div>
-  )
+    <input
+      type={type}
+      placeholder={placeholder}
+      className={`${
+        theme === "dark-mode"
+          ? "bg-gradient-to-br from-[#000000] via-[#2a2a2a] to-[#2d2d2d] p-2 shadow-lg shadow-slate-500 text-white text-xl"
+          : "bg-white p-2 shadow-lg shadow-slate-200 text-black text-xl"
+      } rounded-md`}
+    />
+  );
 }
 
 export default Input
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,30 +1,32 @@
 import React from 'react'
 import { useTheme } from '../context/ThemeContext';
+import Input from '../Components/Input';
 
 const Contact = () => {
     const { theme } = useTheme();
   return (
     <>
       <div>
-        <h2>Have Questions?</h2>
-        <br />
-        <h2>Contact Me Now</h2>
+        <h2 className="text-center">Have Questions?</h2>
+        <div className="flex justify-center my-2">
+          <hr className="border-t-4 border-red-600 w-10" />
+        </div>
+        <h2 className="text-center">Contact Me Now</h2>
         <form action="" className="flex gap-2">
           <div className="w-[49%] flex flex-col gap-2">
-            <input
-              type="text"
-              placeholder="Your Name"
+            <Input type={"text"} placeholder={"Your Name"} />
+            <Input type={"email"} placeholder={"Your email"} />
+            <Input type={"text"} placeholder={"Your Phone"} />
+          </div>
+          <div className="w-full md:w-[49%]">
+            <textarea
+              placeholder="Your Message"
               className={`${
                 theme === "dark-mode"
                   ? "bg-gradient-to-br from-[#000000] via-[#2a2a2a] to-[#2d2d2d] p-2 shadow-lg shadow-slate-500 text-white text-xl"
                   : "bg-white p-2 shadow-lg shadow-slate-200 text-black text-xl"
-              } rounded-md`}
+              } w-full h-full rounded-md`}
             />
-            <input type="email" placeholder="Your Email" />
-            <input type="text" placeholder="Your Phone" />
-          </div>
-          <div className="w-[49%]">
-            <input type="text" name="" id="" />
           </div>
         </form>
       </div>
