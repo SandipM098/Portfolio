90ee3beb358aeec48e81112915fb17bb245d9a5f
Sandip Magar <reone054@gmail.com>
2025-09-04 11:00:22 +0545

    contact submit button correction

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,9 +31,11 @@ const Contact = () => {
               />
             </div>
           </div>
-          <button className="cursor-pointer border font-semibold bg-red-500 py-2 px-5 shadow-md shadow-red-300 rounded-md hover:bg-transparent hover:text-red-500 transition-colors hover:border mt-2 text-white">
-            Submit Form
-          </button>
+          <div className="flex justify-center mt-4">
+            <button className="cursor-pointer border font-semibold bg-red-500 py-2 px-5 shadow-md shadow-red-300 rounded-md hover:bg-transparent hover:text-red-500 transition-colors hover:border text-white">
+              Submit Form
+            </button>
+          </div>
         </form>
       </div>
     </>
